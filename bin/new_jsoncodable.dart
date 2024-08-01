96033d6d0b1f72042868610d176d2e624a96cd42
Koswara <koswara.c79@gmail.com>
2024-08-01 10:46:55 +0700

    get api json

---
+++ bin/new_jsoncodable.dart
@@ -0,0 +1,11 @@
+import 'package:http/http.dart' as http;
+
+void main() async {
+  final httpPackageUrl = Uri.https('dart.dev', '/f/packages/http.json');
+  final httpPackageResponse = await http.get(httpPackageUrl);
+  if (httpPackageResponse.statusCode != 200) {
+    print('Failed to retrieve the http package!');
+    return;
+  }
+  print(httpPackageResponse.body);
+}
